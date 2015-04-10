5f2b2d28d7c33481da6c59674a1970348155550a
Agustino Saling <agustino@saling.eu>
2015-04-10 13:22:21 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Responive Meteor Package of purecss
+
+This package installs purecss with responsive modules.
+
+## Documentation:
+A set of small, responsive CSS modules that you can use in every web project. 
+[http://purecss.io/](http://purecss.io)
+
+## Install
+``` sh
+$ meteor add saling:purecss
+```
---
+++ package.js
@@ -0,0 +1,11 @@
+Package.describe({
+  name: "saling:purecss",
+  summary: "Meteor Package that includes responsive CSS modules for pure.css.",
+  version: "0.6.0",
+  git: "https://github.com/agustino/meteor-purecss.git"
+});
+
+Package.onUse(function (api) {
+  api.versionsFrom('METEOR@1.0');
+  api.addFiles('lib/css/pure-min.css, lib/css/grids-responsive-min.css', 'client');
+});
